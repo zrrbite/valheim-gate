8494d2d34a056285cef3ac64fde8dbca912358c1
Martin Kjeldsen <kjeldsen@gmail.com>
2024-12-08 17:24:45 -0500

    Script to upload both patched valheim.dll and hack

---
+++ Scripts/upload.sh
@@ -0,0 +1,2 @@
+scp ../ICanShowYouTheWorld/bin/Debug/ICanShowYouTheWorld.dll deck@192.168.86.42:/home/deck/.local/share/Steam/steamapps/common/Valheim/valheim_Data/Managed/
+scp ../Patcher/bin/Debug/patched/assembly_valheim.dll deck@192.168.86.42:/home/deck/.local/share/Steam/steamapps/common/Valheim/valheim_Data/Managed/
