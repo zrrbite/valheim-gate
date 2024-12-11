73c01c4167b77c43bb4dc5cf622bbb1b848f1f22
Martin Kjeldsen <kjeldsen@gmail.com>
2024-12-10 20:02:02 -0500

    upload.sh calls _hax.sh and _valheim.sh

--- Scripts/upload.sh
+++ Scripts/upload.sh
@@ -1,2 +1,4 @@
-scp ../ICanShowYouTheWorld/bin/Debug/ICanShowYouTheWorld.dll deck:masteryoda@192.168.86.42:/home/deck/.local/share/Steam/steamapps/common/Valheim/valheim_Data/Managed/
-scp ../Patcher/bin/Debug/patched/assembly_valheim.dll deck@192.168.86.42:/home/deck/.local/share/Steam/steamapps/common/Valheim/valheim_Data/Managed/
+./upload_hax.sh
+./upload_valheim.sh
+#scp ../ICanShowYouTheWorld/bin/Debug/ICanShowYouTheWorld.dll deck:masteryoda@192.168.86.42:/home/deck/.local/share/Steam/steamapps/common/Valheim/valheim_Data/Managed/
+#scp ../Patcher/bin/Debug/patched/assembly_valheim.dll deck@192.168.86.42:/home/deck/.local/share/Steam/steamapps/common/Valheim/valheim_Data/Managed/
--- Scripts/upload_valheim.sh
+++ Scripts/upload_valheim.sh
@@ -1,1 +1,1 @@
-scp ../Patcher/bin/debug/patched/assembly_valheim.dll deck:masteryoda@192.168.86.42:/home/deck/.local/share/Steam/steamapps/common/Valheim/valheim_Data/Managed/
+scp ../Patcher/bin/debug/patched/assembly_valheim.dll deck@192.168.86.42:/home/deck/.local/share/Steam/steamapps/common/Valheim/valheim_Data/Managed/
