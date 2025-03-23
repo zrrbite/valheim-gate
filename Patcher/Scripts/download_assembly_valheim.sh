e8b2f531eb4be6b8034c6f28145c70bf9a12dd62
Martin Kjeldsen <kjeldsen@gmail.com>
2025-03-23 00:08:46 -0400

    Remove old assembly and copy new to patch folder

--- Patcher/Scripts/download_assembly_valheim.sh
+++ Patcher/Scripts/download_assembly_valheim.sh
@@ -1,2 +1,3 @@
 scp deck@192.168.86.42:/home/deck/.local/share/Steam/steamapps/common/Valheim/valheim_Data/Managed/assembly_valheim.dll .
-mv assembly_valheim.dll ../bin/Debug
+rm ../bin/Debug/assembly_valheim.dll.org
+mv assembly_valheim.dll ../bin/Debug/assembly_valheim.dll.org
