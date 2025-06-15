ad8885f665056c788bb24ec3acf603e2409cc5e8
Martin Kjeldsen <kjeldsen@gmail.com>
2025-06-15 19:48:58 -0400

    download and patch in one go

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -1,3 +1,10 @@
 #scp deck@192.168.86.42:/home/deck/.local/share/Steam/steamapps/common/Valheim/valheim_Data/Managed/ICanShowYouTheWorld.dll .  
 scp deck@192.168.86.42:/home/deck/.local/share/Steam/steamapps/common/Valheim/valheim_Data/Managed/assembly_valheim.dll ../libraries  
 scp deck@192.168.86.42:/home/deck/.local/share/Steam/steamapps/common/Valheim/valheim_Data/Managed/UnityEngine.UI.dll ../libraries  
+
+#patch
+cp ../libraries/assembly_valheim.dll ../Patcher/bin/Debug/assembly_valheim.dll.org
+cd ../Patcher/bin/Debug/
+mono Patcher.exe
+
+# ready for upload in /patched/
